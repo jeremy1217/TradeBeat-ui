42d584d6e21474b65551bfde860d244c6244bc55
jhamel76 <jeremy.hamel@dotcms.com>
2025-06-17 15:07:13 -0400

    updated css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import TradeBeatMockups from './components/TradeBeatMockups'
 
 function App() {
